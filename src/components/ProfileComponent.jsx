393788c72d72ff7d2fbf92168df9638d45e07ecb
nastya-atro <belle.nastja@gmail.com>
2021-06-21 11:43:35 +0300

    create profile component

--- src/components/ProfileComponent.jsx
+++ src/components/ProfileComponent.jsx
@@ -1,17 +1,71 @@
 import { observer } from 'mobx-react-lite'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import profile from './../store/Profile'
 
-export const PofileComponent=observer(()=>{
+export const PofileComponent = observer(() => {
 
-    useEffect(()=>{
+    const [visibleEdit, setVisibleEdit] = useState(false)
+    const [name, setName] = useState('')
+    const [vk, setVk] = useState('')
+    const [github, setGithub] = useState('')
+
+    useEffect(() => {
         profile.getProfile()
-    },[])
 
-    return(
+    }, [])
+
+    const changeInfo = () => {
+        profile.changeProfile({
+            userId: 16364,
+            aboutMe: 'z',
+            lookingForAJob: false,
+            lookingForAJobDescription: 'z',
+            fullName: name,
+            contacts: {
+                github: github,
+                vk: vk,
+                facebook: '',
+                instagram: '',
+                twitter: '',
+                website: '',
+                youtube: '',
+                mainLink: ''
+            }
+        })
+
+        setName('')
+        setGithub('')
+        setVk('')
+        setVisibleEdit(false)
+    }
+
+    return (
         <div>
             <h3>Profile info:</h3>
-            {profile.profile && profile.profile.fullName }
+            <button onClick={() => { setVisibleEdit(!visibleEdit) }}>Change profile</button>
+            {!visibleEdit ? <div>
+
+                {profile.profile && <div>
+                    <div><b>Name:</b>{profile.profile.fullName}</div>
+                    <div><b>Contacts:</b>
+                        <div><b>vkontacte:</b>{profile.profile.contacts.vk || '-'}</div>
+                        <div><b>github:</b>{profile.profile.contacts.github || '-'}</div>
+                    </div>
+                </div>}
+
+            </div>
+                :
+                <div>
+                    <input placeholder='name' value={name} onChange={(e) => { setName(e.target.value) }}></input>
+                    <div>
+                        <input placeholder='vk' value={vk} onChange={(e) => { setVk(e.target.value) }}></input>
+                    </div>
+                    <input placeholder='github' value={github} onChange={(e) => { setGithub(e.target.value) }}></input>
+                    <div>
+                        <button onClick={changeInfo}>Save</button></div>
+                </div>
+            }
+            {profile.isLoading ? <div>Saving process...</div> : null}
         </div>
     )
 })
--- src/store/Profile.js
+++ src/store/Profile.js
@@ -1,16 +1,34 @@
 import axios from "axios"
 import { makeAutoObservable } from "mobx"
 
+const instanse = axios.create({
+    withCredentials: true,
+    headers: { "API-KEY": "e33a9b28-32d1-4022-81b8-0bd4ba992caa" },
+    baseURL: `https://social-network.samuraijs.com/api/1.0/`
+})
+
 class Profile {
     profile = null
+    isLoading=false
 
     constructor() {
         makeAutoObservable(this)
     }
 
     getProfile(){
-        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/16364`)
+        instanse.get(`https://social-network.samuraijs.com/api/1.0/profile/16364`)
         .then(res=> this.profile = res.data )
+    }
+
+    changeProfile= async(newInfo)=>{
+        this.isLoading=true
+        const res= await instanse.put(`profile`, newInfo)
+        this.isLoading=false
+        if(res.data.resultCode===0){
+            this.getProfile()
+        } else{
+            console.log('error')
+        }
     }
 }
 
